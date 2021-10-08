b6bb4cabb85f100b59915f7c53d8cf6a6e7df681
zheng hu <zhenghu@umich.edu>
2021-10-07 22:34:57 -0400

    Update encryption password in DB

--- sql/data.sql
+++ sql/data.sql
@@ -1,14 +1,10 @@
 PRAGMA foreign_keys = ON;
 
 insert into users(username,fullname,email,filename,password)
-values ('awdeorio', 'Andrew DeOrio',
-'awdeorio@umich.edu','e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg','password'),
-('jflinn', 'Jason Flinn',
-'jflinn@umich.edu','505083b8b56c97429a728b68f31b0b2a089e5113.jpg','password'),
-('michjc', 'Michael Cafarella',
-'michjc@umich.edu','5ecde7677b83304132cb2871516ea50032ff7a4f.jpg','password'),
-('jag', 'H.V. Jagadish',
-'jag@umich.edu','73ab33bd357c3fd42292487b825880958c595655.jpg','password');
+values ('awdeorio', 'Andrew DeOrio', 'awdeorio@umich.edu', 'e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8'),
+    ('jflinn', 'Jason Flinn', 'jflinn@umich.edu', '505083b8b56c97429a728b68f31b0b2a089e5113.jpg', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8'),
+    ('michjc', 'Michael Cafarella', 'michjc@umich.edu', '5ecde7677b83304132cb2871516ea50032ff7a4f.jpg', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8'),
+    ('jag', 'H.V. Jagadish', 'jag@umich.edu', '73ab33bd357c3fd42292487b825880958c595655.jpg', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8');
 
 
 insert into posts(postid,filename,owner)
